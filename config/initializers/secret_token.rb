8af060ed9de5fa96dd161be4adb1797a7db90a6b
t.daniel <t.daniel@campus.fct.unl.pt>
2013-12-06 23:10:45 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Letsgopro::Application.config.secret_key_base = 'ff8fa85f59a4f1f31ed4357b2bbed82fceb818945966dd8c5889b4e2b0e2687e6cf4e73ece1a131e95299c2afe91cf1bcfe220796d01cc3534ef495c47869b39'
